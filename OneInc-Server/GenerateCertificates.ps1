d7e8b14a076a319f6540623e12dda8c68be3d12e
Alexey Kurochkin <Alexey_Kurochkin1@epam.com>
2023-10-17 00:39:00 +0300

    Fixed script

--- OneInc-Server/GenerateCertificates.ps1
+++ OneInc-Server/GenerateCertificates.ps1
@@ -5,15 +5,10 @@ $password = Read-Host -Prompt "Enter the password" -AsSecureString
 $passwordText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
 
 # Replace the "<PASSWORD_PLACEHOLDER>" with the entered password
-$commands = @(
-    'dotnet dev-certs https -ep $env:USERPROFILE\AppData\Roaming\ASP.NET\Https\aspnetapp.pfx -p ' + $passwordText,
-    'dotnet dev-certs https --trust',
-    'dotnet user-secrets set "Kestrel:Certificates:Development:Password" ' + $passwordText
-)
 
-# Execute the commands
-foreach ($command in $commands) {
-    Invoke-Expression $command
-}
+dotnet dev-certs https -ep $env:USERPROFILE\AppData\Roaming\ASP.NET\Https\OneInc-Server.pfx -p "$passwordText"
+dotnet dev-certs https --trust
+dotnet user-secrets set "Kestrel:Certificates:Development:Password" "$passwordText"
+
 
 Write-Host "Certificates generated successfully."
