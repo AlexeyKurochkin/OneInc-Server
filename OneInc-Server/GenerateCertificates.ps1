c9e2b5a0a530921e98da1a8cd17f0c1f35b6052e
Alexey Kurochkin <Alexey_Kurochkin1@epam.com>
2023-10-16 22:56:40 +0300

    Added script for generating ssl certificate for using https in docker container

---
+++ OneInc-Server/GenerateCertificates.ps1
@@ -0,0 +1,19 @@
+# Prompt the user for a password
+$password = Read-Host -Prompt "Enter the password" -AsSecureString
+
+# Convert the secure string to plain text
+$passwordText = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($password))
+
+# Replace the "<PASSWORD_PLACEHOLDER>" with the entered password
+$commands = @(
+    'dotnet dev-certs https -ep $env:USERPROFILE\AppData\Roaming\ASP.NET\Https\aspnetapp.pfx -p ' + $passwordText,
+    'dotnet dev-certs https --trust',
+    'dotnet user-secrets set "Kestrel:Certificates:Development:Password" ' + $passwordText
+)
+
+# Execute the commands
+foreach ($command in $commands) {
+    Invoke-Expression $command
+}
+
+Write-Host "Certificates generated successfully."
